55e04c0c552deff2515cde6c428463576cc77d15
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-06-23 23:58:25 +0300

    refactor: inherit id type in autohide plugin

--- src/lib/plugins/autohide/autohide.ts
+++ src/lib/plugins/autohide/autohide.ts
@@ -1,15 +1,21 @@
 import { BaseNodeProps } from '$lib/components/scg/node/types'
 import { ReadonlySignal, useSignal, useSignalEffect } from '@preact/signals'
 
-type Props = {
-  nodes: ReadonlySignal<BaseNodeProps[]>
+type Props<Id extends string | number, NodeType extends unknown> = {
+  nodes: ReadonlySignal<BaseNodeProps<Id, NodeType>[]>
   globalize: (x: number, y: number) => readonly [number, number]
   centerX: ReadonlySignal<number>
   centerY: ReadonlySignal<number>
 }
 
-export const withAutohide = (props: Props) => {
-  const nodes = useSignal<BaseNodeProps[]>(props.nodes.peek())
+export type AutohideContext<Id extends string | number, NodeType extends unknown> = {
+  nodes: ReadonlySignal<BaseNodeProps<Id, NodeType>[]>
+}
+
+export const withAutohide = <Id extends string | number, NodeType extends unknown>(
+  props: Props<Id, NodeType>
+): AutohideContext<Id, NodeType> => {
+  const nodes = useSignal<BaseNodeProps<Id, NodeType>[]>(props.nodes.peek())
   useSignalEffect(() => {
     const newNodes = props.nodes.peek().filter(node => {
       const [x, y] = props.globalize(node.x.value, node.y.value)
