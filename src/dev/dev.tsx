b198f17908eb71e9c37292f7e249902dcb04fce4
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-06-24 00:02:44 +0300

    refactor: draggable plugin

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -1,10 +1,11 @@
 import { Alphabet } from '$lib/components/scg/alphabet'
 import { BasicNodeProps, Node } from '$lib/components/scg/node'
-import { BaseNodeProps } from '$lib/components/scg/node/types'
 import { ComponentProps, EnigraphFactory } from '$lib/graph/factory'
 import { withAutohide } from '$lib/plugins/autohide'
+import { withDraggable } from '$lib/plugins/draggable'
 import { withMovable } from '$lib/plugins/movable'
-import { effect, ReadonlySignal, signal, untracked, useComputed, useSignal, useSignalEffect } from '@preact/signals'
+import { AreaSelection, withSelection } from '$lib/plugins/selection'
+import { signal } from '@preact/signals'
 import { render } from 'preact'
 
 const style = signal('background:skyblue')
@@ -15,17 +16,58 @@ const nodeSize = signal(10)
 const factory = new EnigraphFactory()
   .add('node', (props: BasicNodeProps) => <Node {...props} padding={nodePadding} />)
   .plug(withMovable)
+  .plug(withSelection)
+  .plug(() => {
+    const changeNodePosition = (node: BasicNodeProps, x: number, y: number) => {
+      node.x.value = x
+      node.y.value = y
+    }
+    return { changeNodePosition }
+  })
+  .plug(withDraggable)
   .plug(withAutohide)
   .on('graph:wheel', (ctx, e) => ctx.onwheel(e))
-  .configure(_ctx => ({
+  .on('graph:mouseDown', (ctx, e) => {
+    if (e.buttons === 1) {
+      ctx.startSelection(e, {
+        deselection: e.altKey,
+        selection: e.ctrlKey || e.metaKey,
+        clear: !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey,
+      })
+      return
+    }
+  })
+  .on('node:mouseDown', (ctx, e) => {
+    if (e.buttons === 1) {
+      if (e.shiftKey) return
+
+      if (e.ctrlKey || e.metaKey || e.altKey) {
+        e.stopPropagation()
+        return
+      }
+      ctx.startDragginig(e)
+      return
+    }
+  })
+  .on('global:mouseMove', (ctx, e) => {
+    ctx.updateSelection(e, { deselection: e.altKey, selection: e.ctrlKey || e.metaKey })
+    ctx.updateDragging(e)
+  })
+  .on('global:mouseUp', (ctx, _e) => {
+    ctx.stopSelection()
+    ctx.stopDragging()
+  })
+  .configure(ctx => ({
     svgProps: { style },
     staticBefore: [() => <Alphabet size={nodeSize} />],
+    staticAfter: [() => <AreaSelection {...ctx.areaSelection.value} shown={ctx.isSelecting} />],
   }))
 
 const Enigraph = factory.create()
 const nodes = signal<ComponentProps<typeof factory, 'node'>[]>([
-  { id: 0, type: signal('const-tuple'), x: signal(0), y: signal(0), label: signal('hello') },
-  { id: 1, type: signal('var-norole'), x: signal(70), y: signal(50), label: signal('world') },
+  { id: 5, type: signal('const-tuple'), x: signal(0), y: signal(-80), label: signal('bye') },
+  { id: 1, type: signal('const-tuple'), x: signal(70), y: signal(-50), label: signal('cool') },
+  { id: 2, type: signal('const-tuple'), x: signal(0), y: signal(0), label: signal('hello') },
 ])
 
 const result = <Enigraph width={signal(400)} height={signal(400)} nodes={nodes} />
--- src/lib/plugins/draggable/draggable.ts
+++ src/lib/plugins/draggable/draggable.ts
@@ -1,41 +1,46 @@
-import { INode } from '$lib/types'
-import { useSignal } from '@preact/signals'
-import { ReadonlySignal, computed } from '@preact/signals'
-import { DeepSignal } from 'deepsignal'
-import { useCallback, useEffect } from 'preact/hooks'
+import { BaseNodeProps } from '$lib/components/scg/node/types'
+import { ReadonlySignal, useSignal } from '@preact/signals'
 
-type Props = {
-  nodes: DeepSignal<INode[]>
-  selection: ReadonlySignal<Set<number>>
+type Props<Id extends string | number> = {
+  nodes: ReadonlySignal<BaseNodeProps<Id>[]>
+  selection: ReadonlySignal<Set<Id>>
   getInnerPoint: (x: number, y: number) => readonly [number, number]
-  changeNodePosition?(element: INode, x: number, y: number): void
-  nodePositionChanged?(element: INode): void
-  zoom?: ReadonlySignal<number>
+  changeNodePosition?(element: BaseNodeProps<Id>, x: number, y: number): void
+  nodePositionChanged?(element: BaseNodeProps<Id>): void
+  zoom: ReadonlySignal<number>
 }
 
-export const withDraggable = (props: Props) => () => {
-  const dragging = useSignal(false)
+type DraggingContext = {
+  isDragging: ReadonlySignal<boolean>
+  startDragginig: (e: MouseEvent) => void
+  updateDragging: (e: MouseEvent) => void
+  abortDragging: (options?: { revert: boolean }) => void
+  stopDragging: () => void
+}
+
+export const withDraggable = <Id extends string | number>(props: Props<Id>): DraggingContext => {
+  const isDragging = useSignal(false)
   const startPoint = useSignal({ x: 0, y: 0 })
   const totalShift = useSignal({ x: 0, y: 0 })
 
   const startDragginig = (e: MouseEvent) => {
-    dragging.value = true
+    isDragging.value = true
     const [x, y] = props.getInnerPoint(e.clientX, e.clientY)
     startPoint.value.x = x
     startPoint.value.y = y
     totalShift.value.x = 0
     totalShift.value.y = 0
-    document.addEventListener('mouseup', stopDragging, { once: true })
   }
 
   const updateDragging = (e: MouseEvent) => {
+    if (!isDragging.value) return
     const [x, y] = props.getInnerPoint(e.clientX, e.clientY)
     const shiftX = startPoint.value.x - x
     const shiftY = startPoint.value.y - y
     const zoom = props.zoom?.value ?? 1
-    for (const node of props.nodes) {
+    for (const node of props.nodes.value) {
       if (!props.selection.value.has(node.id)) continue
-      props.changeNodePosition?.(node, node.x - shiftX / zoom, node.y - shiftY / zoom)
+      props.changeNodePosition?.(node, node.x.value - shiftX / zoom, node.y.value - shiftY / zoom)
     }
     totalShift.value.x += shiftX
     totalShift.value.y += shiftY
@@ -44,32 +49,31 @@ export const withDraggable = (props: Props) => () => {
   }
 
   const abortDragging = (options?: { revert: boolean }) => {
-    if (!dragging.value) return
+    if (!isDragging.value) return
     if (options?.revert) {
       const zoom = props.zoom?.value ?? 1
-      for (const node of props.nodes) {
+      for (const node of props.nodes.value) {
         if (!props.selection.value.has(node.id)) continue
-        props.changeNodePosition?.(node, node.x + totalShift.value.x / zoom, node.y + totalShift.value.y / zoom)
+        props.changeNodePosition?.(
+          node,
+          node.x.value + totalShift.value.x / zoom,
+          node.y.value + totalShift.value.y / zoom
+        )
       }
     }
     stopDragging()
   }
 
-  const stopDragging = useCallback(() => {
-    document.removeEventListener('mouseup', stopDragging)
+  const stopDragging = () => {
+    if (!isDragging.value) return
     if (props.nodePositionChanged) {
-      for (const node of props.nodes) {
+      for (const node of props.nodes.value) {
         if (!props.selection.value.has(node.id)) continue
         props.nodePositionChanged(node)
       }
     }
-    dragging.value = false
-  }, [dragging, props.nodes, props.selection, props.nodePositionChanged])
+    isDragging.value = false
+  }
 
-  useEffect(
-    () => () => document.removeEventListener('mouseup', stopDragging),
-    [dragging, props.nodes, props.selection, props.nodePositionChanged]
-  )
-
-  return { isDragging: computed(() => dragging.value), startDragginig, updateDragging, abortDragging }
+  return { isDragging, startDragginig, updateDragging, abortDragging, stopDragging }
 }
