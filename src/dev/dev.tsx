0aa9e18e77a1e3868dbe5681104019a9730fa6f3
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-07-18 16:34:14 +0300

    refactor: add buses and contents generation to dev page

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -3,37 +3,25 @@ import { render } from 'preact'
 import { DemoComponentProps, EnigraphDemo } from './graph-demo'
 import './style.css'
 
-const N = 10
-const E = 12
-const B = 6
+const GR = 1
+const GW = 1000
+const GH = 600
+
+const N = 20
+const E = 50
+const B = 0
+const C = 0
 
 const label = signal('bye')
 const nodes = signal<DemoComponentProps<'node'>>(
   Array.from({ length: N }, (_, i) => ({
     id: i + 1,
     type: signal('const-tuple'),
-    x: signal(Math.round(Math.random() * 1000) - 500),
-    y: signal(Math.round(Math.random() * 600) - 300),
+    x: signal(Math.round(Math.random() * GW * GR) - GW * GR / 2),
+    y: signal(Math.round(Math.random() * GH * GR) - GH * GR / 2),
     label,
   }))
 )
-
-const edges = signal<DemoComponentProps<'edge'>>([])
-Array.from({ length: E }, (_, i) => {
-  const n1 = nodes.value[Math.trunc(Math.random() * N)]
-  const e2 =
-    !i || Math.random() > 0.7 ? nodes.value[Math.trunc(Math.random() * N)] : edges.value[Math.trunc(Math.random() * i)]
-  edges.value.push({
-    id: i + 1,
-    type: signal('const-tuple'),
-    x1: n1.x,
-    y1: n1.y,
-    x2: 'x' in e2 ? e2.x : computed(() => (e2.x2.value - e2.x1.value) / 2 + e2.x1.value),
-    y2: 'y' in e2 ? e2.y : computed(() => (e2.y2.value - e2.y1.value) / 2 + e2.y1.value),
-    sourceId: n1.id,
-    targetId: e2.id,
-  })
-})
 
 const buss = signal<DemoComponentProps<'bus'>>([])
 Array.from({ length: B }, (_, i) => {
@@ -43,8 +31,49 @@ Array.from({ length: B }, (_, i) => {
     sourceId: n1.id,
     x: n1.x,
     y: n1.y,
-    dx: signal(Math.round(Math.random() * 1000) - 500),
-    dy: signal(Math.round(Math.random() * 600) - 300),
+    dx: signal(Math.round(Math.random() * GW * GR) - GW * GR / 2),
+    dy: signal(Math.round(Math.random() * GH * GR) - GH * GR / 2),
+  })
+})
+
+const contents = signal<DemoComponentProps<'content'>>([])
+Array.from({ length: C }, (_, i) => {
+  const { type, value } =
+    Math.random() > 0.5
+      ? {
+          type: 'image' as const,
+          value:
+            'https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?cs=srgb&dl=pexels-8moments-1266810.jpg&fm=jpg',
+        }
+      : { type: 'text' as const, value: 'hellooooooooooooooo there' }
+  contents.value.push({
+    id: i + 1,
+    type: signal(type),
+    value: signal(value),
+    x: signal(Math.round(Math.random() * GW * GR) - GW * GR / 2),
+    y: signal(Math.round(Math.random() * GH * GR) - GH * GR / 2),
+    dx: signal(200),
+    dy: signal(200),
+  })
+})
+
+const edges = signal<DemoComponentProps<'edge'>>([])
+Array.from({ length: E }, (_, i) => {
+  const n1 = nodes.value[Math.trunc(Math.random() * N)]
+  const isNode = !i || Math.random() > 0.7
+  const e2 = isNode
+    ? nodes.value[Math.trunc(Math.random() * N)]
+    : contents.value[Math.trunc(((Math.random() * i) / N) * C)]
+  if (!e2) return
+  edges.value.push({
+    id: i + 1,
+    type: signal('const-tuple'),
+    x1: n1.x,
+    y1: n1.y,
+    x2: 'dx' in e2 ? computed(() => e2.x.value + e2.dx.value / 2 + 3.2) : e2.x,
+    y2: 'dy' in e2 ? computed(() => e2.y.value + e2.dy.value / 2 + 3.2) : e2.y,
+    sourceId: n1.id,
+    targetId: e2.id,
   })
 })
 
@@ -55,7 +84,7 @@ const App = () => {
         <h2>Playground</h2>
         <button>Clear</button>
       </div>
-      <EnigraphDemo nodes={nodes} edges={edges} buss={buss} />
+      <EnigraphDemo nodes={nodes} edges={edges} buss={buss} contents={contents} />
     </div>
   )
 }
